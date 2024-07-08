b01a5948466db6760ccb71d3002a352296a04ce9
liudengfeng <liudengfeng_sd@outlook.com>
2024-07-08 20:23:41 +0800

    feat: Enable Google Generative AI integration for content generation

---
+++ README.md
@@ -0,0 +1,37 @@
+### install package
+
+```
+npm install
+```
+
+### Develop with Wrangler CLI
+
+```
+npx wrangler dev
+```
+
+### Deploy your project
+
+```
+npx wrangler deploy
+```
+
+
+## wrangler
+
+### Save your API key to your Workers env
+
+```
+npx wrangler secret put <OPENAI_API_KEY>
+```
+
+### List all secrets for a Worker
+
+```
+npx wrangler secret list
+```
+
+
+```
+
+```
--- src/index.js
+++ src/index.js
@@ -10,42 +10,37 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		// const { GoogleGenerativeAI } = require("@google/generative-ai");
+		const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 		// const openai = new OpenAI({
 		// 	apiKey: env.OPENAI_API_KEY,
 		// 	baseURL: "https://gateway.ai.cloudflare.com/v1/{account_id}/{gateway_id}/openai"
 		// });
-		// console.log("process GOOGLE AI API KEY:");
-		// console.log(process.env.GOOGLE_AI_API_KEY);
-		console.log("GOOGLE AI API KEY:");
-		console.log(env.GOOGLE_AI_API_KEY);
 
-		// try {
-		// 	// Access your API key as an environment variable (see "Set up your API key" above)
-		// 	// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-		// 	const genAI = new GoogleGenerativeAI(env.GOOGLE_AI_API_KEY);
-		// 	const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-		// 	// const chatCompletion = await openai.chat.completions.create({
-		// 	// 	model: "gpt-3.5-turbo-0613",
-		// 	// 	messages: [{ role: "user", content: "What is a neuron?" }],
-		// 	// 	max_tokens: 100,
-		// 	// });
+		try {
+			// Access your API key as an environment variable (see "Set up your API key" above)
+			// const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+			const genAI = new GoogleGenerativeAI(env.GOOGLE_AI_API_KEY);
+			const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+			// const chatCompletion = await openai.chat.completions.create({
+			// 	model: "gpt-3.5-turbo-0613",
+			// 	messages: [{ role: "user", content: "What is a neuron?" }],
+			// 	max_tokens: 100,
+			// });
 
-		// 	// const response = chatCompletion.choices[0].message;
+			// const response = chatCompletion.choices[0].message;
 
-		// 	// return new Response(JSON.stringify(response));
+			// return new Response(JSON.stringify(response));
 
-		// 	const prompt = "Write a story about a AI and magic"
-		// 	const result = await model.generateContent(prompt);
-		// 	const response = await result.response;
-		// 	const text = response.text();
-		// 	// return new Response(JSON.stringify(text));
-		// 	return new Response(text);
+			const prompt = "Write a story about a AI and magic"
+			const result = await model.generateContent(prompt);
+			const response = await result.response;
+			const text = response.text();
+			// return new Response(JSON.stringify(text));
+			return new Response(text);
 
-		// } catch (e) {
-		// 	return new Response(e);
-		// }
-		return new Response("GOOGLE AI API KEY");
+		} catch (e) {
+			return new Response(e);
+		}
 	},
 };
